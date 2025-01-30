548b6c23900f9ec23aeff4fd23a7a8c2911c042d
tiropp <florian.huber@tiropp.ch>
2025-01-30 16:32:15 +0100

    Create new lein app

---
+++ test/terra_tracker_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns terra-tracker-api.core-test
+  (:require [clojure.test :refer :all]
+            [terra-tracker-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
